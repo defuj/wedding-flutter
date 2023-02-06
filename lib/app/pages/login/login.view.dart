ff221ef7294db032c824c221c76e7863b35b101e
Dede Fuji Abdul <defuj.official@gmail.com>
2023-02-06 13:53:43 +0700

    update

--- lib/app/pages/login/login.view.dart
+++ lib/app/pages/login/login.view.dart
@@ -19,92 +19,92 @@ class _View extends StatelessView<LoginViewModel> {
   Widget render(context, viewModel) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 24),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Image.asset(
-                    'assets/images/component-1.png',
-                  ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 24),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Image.asset(
+                  'assets/images/component-1.png',
                 ),
-                SizedBox(
-                  width: 200,
-                  child: Stack(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          left: 16,
-                          right: 16,
-                          bottom: 30,
-                        ),
-                        child: AspectRatio(
-                          aspectRatio: 1 / 1,
-                          child: ClipOval(
-                            child: Image.network(
-                                'https://cf.shopee.co.id/file/a462f22068f1cc83693c9d1773c7679a'),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 0,
-                        left: 0,
-                        right: 0,
-                        child: Image.asset(
-                          'assets/images/component-3.png',
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Image.asset(
-                    'assets/images/component-2.png',
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
-                'Muhammad Syauqi Alsunni, S.Sos.',
-                textAlign: TextAlign.center,
-                style: Theme.of(context)
-                    .textTheme
-                    .headline3!
-                    .copyWith(color: Colors.black),
               ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              '&',
+              SizedBox(
+                width: 200,
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 16,
+                        right: 16,
+                        bottom: 30,
+                      ),
+                      child: AspectRatio(
+                        aspectRatio: 1 / 1,
+                        child: ClipOval(
+                          child: Image.network(
+                              'https://cf.shopee.co.id/file/a462f22068f1cc83693c9d1773c7679a'),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Image.asset(
+                        'assets/images/component-3.png',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Image.asset(
+                  'assets/images/component-2.png',
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              'Muhammad Syauqi Alsunni, S.Sos.',
               textAlign: TextAlign.center,
               style: Theme.of(context)
                   .textTheme
                   .headline3!
-                  .copyWith(color: IColors.pink50),
+                  .copyWith(color: Colors.black),
             ),
-            const SizedBox(height: 8),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
-                'Puti Kayo Gebriecya, B.Sc.',
-                textAlign: TextAlign.center,
-                style: Theme.of(context)
-                    .textTheme
-                    .headline3!
-                    .copyWith(color: Colors.black),
-              ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            '&',
+            textAlign: TextAlign.center,
+            style: Theme.of(context)
+                .textTheme
+                .headline3!
+                .copyWith(color: IColors.pink50),
+          ),
+          const SizedBox(height: 8),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              'Puti Kayo Gebriecya, B.Sc.',
+              textAlign: TextAlign.center,
+              style: Theme.of(context)
+                  .textTheme
+                  .headline3!
+                  .copyWith(color: Colors.black),
             ),
-            Container(
+          ),
+          Expanded(
+            child: Container(
               padding: const EdgeInsets.all(16),
               margin: const EdgeInsets.only(top: 32),
               decoration: const BoxDecoration(
@@ -167,8 +167,8 @@ class _View extends StatelessView<LoginViewModel> {
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
